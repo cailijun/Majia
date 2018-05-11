8de5aa37af8cf0564971d994af17dec8f61fc51e
cailj <cailj@fxtcn.com>
2018-05-11 13:54:58 +0800

    Merge branch '1.0Branch'
    # Conflicts:
    #	app/src/main/java/activity/mj/com/helloma/FirstActivity.java

--- app/src/main/java/activity/mj/com/helloma/FirstActivity.java
+++ app/src/main/java/activity/mj/com/helloma/FirstActivity.java
@@ -25,6 +25,7 @@ public class FirstActivity extends Activity{
         setContentView(R.layout.activity_first);
         initData();
         System.out.print("1.0版本的修改");
+        System.out.print("2.0devolop");
     }
 
     private void initData() {
