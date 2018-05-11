91ecfe9476d60bbc3de9ff6e6564e9ad6c52f9cd
cailj <cailj@fxtcn.com>
2018-05-11 11:01:45 +0800

    5.0提交

--- app/src/main/java/activity/mj/com/helloma/TestActivity.java
+++ app/src/main/java/activity/mj/com/helloma/TestActivity.java
@@ -1,8 +1,17 @@
 package activity.mj.com.helloma;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by pc on 2018/5/11.
  */
 
-public class TestActivity {
+public class TestActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.print("test");
+    }
 }
