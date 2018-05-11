8d7a641dfb84a85c18c4a333f90d7c2c3b021ca1
cailj <cailj@fxtcn.com>
2018-05-11 17:31:52 +0800

    修改first

--- app/src/main/java/activity/mj/com/helloma/FirstActivity.java
+++ app/src/main/java/activity/mj/com/helloma/FirstActivity.java
@@ -24,8 +24,7 @@ public class FirstActivity extends Activity{
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_first);
         initData();
-        System.out.print("1111");
-
+       System.out.print("F:majia.1111111111");
     }
 
     private void initData() {
