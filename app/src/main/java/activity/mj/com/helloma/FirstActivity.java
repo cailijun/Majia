2c8ed278d7e8959138ba8a28d9d1400d11c1b5f7
cailijun <290600938@qq.com>
2018-05-14 17:29:06 +0800

    Update FirstActivity.java
    111

--- app/src/main/java/activity/mj/com/helloma/FirstActivity.java
+++ app/src/main/java/activity/mj/com/helloma/FirstActivity.java
@@ -24,7 +24,7 @@ public class FirstActivity extends Activity{
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_first);
         initData();
-
+        System.out.printf("111");
     }
 
     private void initData() {
