0775cf70815cee9c5c59a6e14d8c22c5d98ea4ab
cailj <cailj@fxtcn.com>
2018-05-14 17:11:47 +0800

    修改了first,去除了注释

--- app/src/main/java/activity/mj/com/helloma/FirstActivity.java
+++ app/src/main/java/activity/mj/com/helloma/FirstActivity.java
@@ -24,8 +24,7 @@ public class FirstActivity extends Activity{
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_first);
         initData();
-        System.out.print("1111");
-          System.out.print("2222222222");
+
     }
 
     private void initData() {
