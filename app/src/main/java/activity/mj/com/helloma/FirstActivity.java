2ca8b49b083d32c1ae7a85ab7028d81f9d3eddec
cailj <cailj@fxtcn.com>
2018-05-14 18:04:35 +0800

    新增注释111

--- app/src/main/java/activity/mj/com/helloma/FirstActivity.java
+++ app/src/main/java/activity/mj/com/helloma/FirstActivity.java
@@ -28,7 +28,7 @@ public class FirstActivity extends Activity{
     }
 
     private void initData() {
-        System.out.printf("2222");
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://1114600.com:8080/")
                 .addConverterFactory(GsonConverterFactory.create())
